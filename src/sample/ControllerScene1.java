4b63c9a418a8108378568a8da4d7b0484b331a0f
Troels21 <73639990+Troels21@users.noreply.github.com>
2021-02-23 15:08:58 +0100

    1508 Troels

--- src/sample/ControllerScene1.java
+++ src/sample/ControllerScene1.java
@@ -1,9 +1,14 @@
 package sample;
 
 import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.chart.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,63 +19,65 @@ import java.util.concurrent.TimeUnit;
 public class ControllerScene1 {
     Main m = new Main();
     Beregner b = new Beregner();
-    int counter;
-    ScheduledExecutorService andreas = Executors.newSingleThreadScheduledExecutor();
+    ScheduledExecutorService pulsEventhandler = Executors.newSingleThreadScheduledExecutor();
+    XYChart.Series pulsSerie = new XYChart.Series();
+    String name;
+
+    @FXML
+        LineChart<CategoryAxis, NumberAxis> Diagram;
+    @FXML
+        TextField Name;
+
 
     public void monitorStart() throws IOException {
-        b.switchpulse =true;
-        XYChart.Series pulsSerie = new XYChart.Series();
         pulsSerie.setName("puls");
-
-
-        andreas.scheduleAtFixedRate(()->
-                Platform.runLater(()->{
+        Diagram.getData().add(pulsSerie);
+        pulsEventhandler.scheduleAtFixedRate(() ->
+                Platform.runLater(() -> {
                     String bogstav = String.valueOf(b.i);
                     b.pulsSimulering();
                     pulsSerie.getData().add(new XYChart.Data(bogstav, b.puls));
-                }),0,1,TimeUnit.SECONDS);
-        /*
-        final CategoryAxis xAxis = new CategoryAxis(); // we are gonna plot against time
-        final NumberAxis yAxis = new NumberAxis();
-        xAxis.setLabel("Time/s");
-        xAxis.setAnimated(false); // axis animations are removed
-        yAxis.setLabel("Value");
-        yAxis.setAnimated(false);
-
-        final LineChart<String, Number> lineChart = new LineChart<>(xAxis, yAxis);
-        lineChart.setTitle("Realtime JavaFX Charts");
-        lineChart.setAnimated(false);
-        lineChart.setTitle("Stock Monitoring, 2010");
-
-        XYChart.Series pulsSerie = new XYChart.Series();
-        pulsSerie.setName("puls");
-
-        Scene testScene = new Scene(lineChart,600,600);*/
-        lineChart.getData().add(pulsSerie);
-        Stage stage2 = new Stage();
-        stage2.setTitle("hello");
-        stage2.setScene(testScene);
-        stage2.show();
-
-        ScheduledExecutorService andreas = Executors.newSingleThreadScheduledExecutor();
-
-        andreas.scheduleAtFixedRate(()->
-            Platform.runLater(()->{
-                String bogstav = String.valueOf(b.i);
-                b.pulsSimulering();
-                pulsSerie.getData().add(new XYChart.Data(bogstav, b.puls));
-            }),0,1,TimeUnit.SECONDS);
+                }), 0, 1, TimeUnit.SECONDS);
     }
 
-    public void saveData(){
+    public void monitorStop() {
+        pulsEventhandler.shutdown();
+    }
+
+    public void saveData() {
+        String name2 =Name.getText();
+        System.out.println(name2);
+
+        if (name2.equals("") || name2.equals("Set Name")){
+            Label alertLabel = new Label();
+            StackPane allertLayout= new StackPane();
+            Stage allertStage = new Stage();
+            Button allertButton = new Button();
+
+            allertButton.setText("OK");
+            alertLabel.setText("Invalid Name");
+            allertStage.setTitle("Alert");
+
+            allertButton.setOnAction(e ->allertStage.close());
+            allertLayout.getChildren().addAll(allertButton,alertLabel);
+            Scene allertScene = new Scene(allertLayout, 200,100);
+            alertLabel.setTranslateY(-25);
+
+            allertStage.setScene(allertScene);
+            allertStage.initModality(Modality.APPLICATION_MODAL);
+            allertStage.show();
+        }
 
     }
 
-    public void monitorStop(){
-        b.stopPulse();
+    public void setName() {
+        name="";
+        //et eller andet med arkiv navn, skal man kunne sætte her.
     }
 
-    public void setName(){
-        System.out.println("det virker");
+    public void refresh() {
+        Diagram.getData().clear();
+        pulsSerie.getData().clear();
+
     }
 }
