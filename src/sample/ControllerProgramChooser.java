03803fe002263acf4b50872e2f911c1a11841364
Schasti <imamshah19@yahoo.com>
2021-03-02 14:40:32 +0100

    array fejl skal fiks

--- src/sample/ControllerProgramChooser.java
+++ src/sample/ControllerProgramChooser.java
@@ -20,4 +20,9 @@ public class ControllerProgramChooser {
         m.stage.setScene(new Scene(ControllerArkiv, 650, 400));
         m.stage.show();
     }
+    public void ekgClicked() throws IOException {
+        Parent EKG = FXMLLoader.load(getClass().getResource("EKG.fxml"));
+        m.stage.setScene(new Scene(EKG, 650, 400));
+        m.stage.show();
+    }
 }
