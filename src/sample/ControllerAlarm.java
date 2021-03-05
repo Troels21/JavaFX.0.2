c9ae17923568ed2bd3e8470b3d284696b0756f5d
Troels21 <73639990+Troels21@users.noreply.github.com>
2021-03-05 13:44:07 +0100

    Lavet Alarm- Troels 05-03-2021 13:43

--- src/sample/ControllerAlarm.java
+++ src/sample/ControllerAlarm.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -8,15 +9,11 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-public class ControllerAlarm {
-    double pulseMaxDouble;
-    double pulseMinDouble;
-    double tempMaxDouble;
-    double tempMinDouble;
-    double SpO2MaxDouble;
-    double SpO2MinDouble;
-    double ekgMaxDouble;
-    double ekgMinDouble;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerAlarm implements Initializable {
+    Main m = new Main();
 
     public TextField pulseMax;
     public TextField pulseMin;
@@ -26,70 +23,52 @@ public class ControllerAlarm {
     public TextField SpO2Min;
     public TextField ekgMax;
     public TextField ekgMin;
-    private int a=0;
+    private int a = 0;
 
     public void updateAlarm() {
-        a=0;
-        check(pulseMax,pulseMin);
-        check(tempMax,tempMin);
-        check(SpO2Max,SpO2Min);
-        check(ekgMax,ekgMin);
+        a = 0;
+        check(pulseMax, pulseMin);
+        check(tempMax, tempMin);
+        check(SpO2Max, SpO2Min);
+        check(ekgMax, ekgMin);
 
-        if (a==4) {
-            pulseMaxDouble = Double.parseDouble(pulseMax.getText());
-            pulseMinDouble = Double.parseDouble(pulseMin.getText());
-            tempMaxDouble = Double.parseDouble(tempMax.getText());
-            tempMinDouble = Double.parseDouble(tempMin.getText());
-            SpO2MaxDouble = Double.parseDouble(SpO2Max.getText());
-            SpO2MinDouble = Double.parseDouble(SpO2Min.getText());
-            ekgMaxDouble = Double.parseDouble(ekgMax.getText());
-            ekgMinDouble = Double.parseDouble(ekgMin.getText());
+        if (a == 4) {
+            m.pulseMaxDouble = Double.parseDouble(pulseMax.getText());
+            m.pulseMinDouble = Double.parseDouble(pulseMin.getText());
+            m.tempMaxDouble = Double.parseDouble(tempMax.getText());
+            m.tempMinDouble = Double.parseDouble(tempMin.getText());
+            m.SpO2MaxDouble = Double.parseDouble(SpO2Max.getText());
+            m.SpO2MinDouble = Double.parseDouble(SpO2Min.getText());
+            m.ekgMaxDouble = Double.parseDouble(ekgMax.getText());
+            m.ekgMinDouble = Double.parseDouble(ekgMin.getText());
 
-            System.out.println(pulseMaxDouble);
-            System.out.println(pulseMinDouble);
-            System.out.println(tempMaxDouble);
-            System.out.println(tempMinDouble);
-            System.out.println(SpO2MaxDouble);
-            System.out.println(SpO2MinDouble);
-            System.out.println(ekgMaxDouble);
-            System.out.println(ekgMinDouble);
+            System.out.println(m.pulseMaxDouble);
+            System.out.println(m.pulseMinDouble);
+            System.out.println(m.tempMaxDouble);
+            System.out.println(m.tempMinDouble);
+            System.out.println(m.SpO2MaxDouble);
+            System.out.println(m.SpO2MinDouble);
+            System.out.println(m.ekgMaxDouble);
+            System.out.println(m.ekgMinDouble);
+
+            setText();
         }
     }
 
-    public void check(TextField text,TextField text2) {
-            try {
-                double max = Double.parseDouble(text.getText());
-                double min= Double.parseDouble(text2.getText());
-                if (max>min){
-                    a++;
-                }
-                else{
-                    Label alertLabel = new Label();
-                    StackPane allertLayout = new StackPane();
-                    Stage allertStage = new Stage();
-                    Button allertButton = new Button();
-
-                    allertButton.setText("OK");
-                    alertLabel.setText("Max has to be greater than Min");
-                    allertStage.setTitle("Alert");
-
-                    allertButton.setOnAction(p -> allertStage.close());
-                    allertLayout.getChildren().addAll(allertButton, alertLabel);
-                    Scene allertScene = new Scene(allertLayout, 200, 100);
-                    alertLabel.setTranslateY(-25);
-
-                    allertStage.setScene(allertScene);
-                    allertStage.initModality(Modality.APPLICATION_MODAL);
-                    allertStage.show();
-                }
-            } catch (NumberFormatException e) {
+    public void check(TextField text, TextField text2) {
+        try {
+            double max = Double.parseDouble(text.getText());
+            double min = Double.parseDouble(text2.getText());
+            if (max > min) {
+                a++;
+            } else {
                 Label alertLabel = new Label();
                 StackPane allertLayout = new StackPane();
                 Stage allertStage = new Stage();
                 Button allertButton = new Button();
 
                 allertButton.setText("OK");
-                alertLabel.setText("Invalid Number, Write a real number comma with . ");
+                alertLabel.setText("Max has to be greater than Min");
                 allertStage.setTitle("Alert");
 
                 allertButton.setOnAction(p -> allertStage.close());
@@ -101,5 +80,41 @@ public class ControllerAlarm {
                 allertStage.initModality(Modality.APPLICATION_MODAL);
                 allertStage.show();
             }
+        } catch (NumberFormatException e) {
+            Label alertLabel = new Label();
+            StackPane allertLayout = new StackPane();
+            Stage allertStage = new Stage();
+            Button allertButton = new Button();
+
+            allertButton.setText("OK");
+            alertLabel.setText("Invalid Number, Write a real number comma with . ");
+            allertStage.setTitle("Alert");
+
+            allertButton.setOnAction(p -> allertStage.close());
+            allertLayout.getChildren().addAll(allertButton, alertLabel);
+            Scene allertScene = new Scene(allertLayout, 200, 100);
+            alertLabel.setTranslateY(-25);
+
+            allertStage.setScene(allertScene);
+            allertStage.initModality(Modality.APPLICATION_MODAL);
+            allertStage.show();
         }
     }
+
+
+    public void setText() {
+        pulseMax.setText(String.valueOf(m.pulseMaxDouble));
+        pulseMin.setText(String.valueOf(m.pulseMinDouble));
+        tempMax.setText(String.valueOf(m.tempMaxDouble));
+        tempMin.setText(String.valueOf(m.tempMinDouble));
+        SpO2Max.setText(String.valueOf(m.SpO2MaxDouble));
+        SpO2Min.setText(String.valueOf(m.SpO2MinDouble));
+        ekgMax.setText(String.valueOf(m.ekgMaxDouble));
+        ekgMin.setText(String.valueOf(m.ekgMinDouble));
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setText();
+    }
+}
