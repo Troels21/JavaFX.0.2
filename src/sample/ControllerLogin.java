dfd9d2e1464996fa6c24e588bfdc3fb8b3059be3
BachBerg <andreas-bach-nielsen@hotmail.com>
2021-03-07 12:58:25 +0100

    07 03 2021 - Andreas Bach, Reformat code

--- src/sample/ControllerLogin.java
+++ src/sample/ControllerLogin.java
@@ -1,17 +1,66 @@
 package sample;
 
+import javafx.fxml.*;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import java.io.IOException;
 
 public class ControllerLogin {
     Main m = new Main();
 
+    @FXML
+    TextField Username;
+    @FXML
+    PasswordField Password;
+
+
     public void login() throws IOException {
-        Parent ProgramChooser = FXMLLoader.load(getClass().getResource("ProgramChooser.fxml"));
-        m.stage.setScene(new Scene(ProgramChooser, 650, 400));
-        m.stage.show();
+
+        if (KontrolSP()) {
+            Parent ProgramChooser = FXMLLoader.load(getClass().getResource("ProgramChooser.fxml"));
+            m.stage.setScene(new Scene(ProgramChooser, 650, 400));
+            m.stage.show();
+        } else if (KontrolL()) {
+            Parent ProgramChooser = FXMLLoader.load(getClass().getResource("ProgramChooser.fxml"));
+            m.stage.setScene(new Scene(ProgramChooser, 650, 400));
+            m.stage.show();
+        } else {
+            Parent ProgramChooser = FXMLLoader.load(getClass().getResource("ProgramChooser.fxml"));
+            m.stage.setScene(new Scene(ProgramChooser, 650, 400));
+            m.stage.show();
+        }
+    }
+
+    private boolean KontrolL() {
+        String[] arkiv = new String[]{"dude", "bro", "guy", "friend", "pal"};
+        String U = Username.getText();
+        String P = Password.getText();
+
+        for (int i = 0; i < arkiv.length - 1; i++) {
+            if (U.equals(arkiv[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean KontrolSP() {
+        String[] arkiv = new String[]{"dude", "bro", "guy", "friend", "pal"};
+        String U = Username.getText();
+        String P = Password.getText();
+
+        for (int i = 0; i < arkiv.length - 1; i++) {
+            if (U.equals(arkiv[i])) {
+                return true;
+            }
+        }
+        return false;
     }
 }
+
+
+
+
