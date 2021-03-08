1269dc27659f290d807aeffdf14cfadd5adf2c89
shivgopal <shiv20@live.dk>
2021-03-08 21:28:46 +0100

    Shiv - 21:28 08/03-21

--- src/sample/ControllerArkiv.java
+++ src/sample/ControllerArkiv.java
@@ -2,18 +2,49 @@ package sample;
 
 import javafx.fxml.*;
 import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
 import java.util.*;
 import java.io.*;
 
 public class ControllerArkiv {
     @FXML
     TextField CPR;
+    @FXML
+    LineChart<CategoryAxis, NumberAxis> Pulse;
+    LineChart<CategoryAxis, NumberAxis> Temp;
+
+    XYChart.Series Pulse1 = new XYChart.Series();
+    XYChart.Series Temperatur1 = new XYChart.Series();
+
+    int[] PulseTime;
+    int[] PulseValue;
+    int[] TempTime;
+    int[] TempValue;
+
+
+    public void PulsArkiv() {
+        Pulse.getData().add(Pulse1);
+        if (PulseTime != null && PulseValue != null) {
+            Pulse1.getData().add(new XYChart.Data(PulseTime, PulseValue));
+        }
+        System.out.println("Virker");
+    }
+
+    public void TempArkiv(){
+
+    }
+
     public void PatientChooser() throws FileNotFoundException {
             File checker = new File("PatientData",CPR.getText());
 
@@ -26,48 +57,32 @@ public class ControllerArkiv {
                 RåPuls = RåPuls.replaceAll("[^0-9,]", "");
                 RåTemp = RåTemp.replaceAll("[^0-9,]", "");
 
-
                 String[] Pulse = RåPuls.split(",");
 
-                int[] PulseTime = new int[Pulse.length/2];
-                if(Pulse.length > 1)
-                {
-                    for (int i = 0; i < Pulse.length; i = i + 2)
-                    {
-                        PulseTime[i / 2] = Integer.parseInt(Pulse[i]);
-                    }
+                PulseTime = new int[Pulse.length/2];
+                if(Pulse.length > 1) {
+                    for (int i = 0; i < Pulse.length; i = i + 2) {
+                        PulseTime[i / 2] = Integer.parseInt(Pulse[i]); }
                 }
-
-                int[] PulseValue = new int[Pulse.length/2];
-                if(Pulse.length > 1)
-                {
-                    for (int i = 1; i < Pulse.length; i = i + 2)
-                    {
-                        PulseValue[i / 2] = Integer.parseInt(Pulse[i]);
-                    }
+                PulseValue = new int[Pulse.length/2];
+                if(Pulse.length > 1) {
+                    for (int i = 1; i < Pulse.length; i = i + 2) {
+                        PulseValue[i / 2] = Integer.parseInt(Pulse[i]); }
                 }
 
                 String[] Temp = RåTemp.split(",");
 
-                int[] TempTime = new int[Temp.length/2];
-                if(Temp.length > 1)
-                {
-                    for (int i = 0; i < Temp.length; i = i + 2)
-                    {
-                        TempTime[i / 2] = Integer.parseInt(Temp[i]);
-                    }
+                TempTime = new int[Temp.length/2];
+                if(Temp.length > 1) {
+                    for (int i = 0; i < Temp.length; i = i + 2) {
+                        TempTime[i / 2] = Integer.parseInt(Temp[i]); }
                 }
 
-                int[] TempValue = new int[Temp.length/2];
-                if(Temp.length > 1)
-                {
-                    for (int i = 1; i < Temp.length; i = i + 2)
-                    {
-                        TempValue[i / 2] = Integer.parseInt(Temp[i]);
-                    }
+                TempValue = new int[Temp.length/2];
+                if(Temp.length > 1) {
+                    for (int i = 1; i < Temp.length; i = i + 2) {
+                        TempValue[i / 2] = Integer.parseInt(Temp[i]); }
                 }
-
-
             }
             else {
                 Label alertLabel = new Label();
