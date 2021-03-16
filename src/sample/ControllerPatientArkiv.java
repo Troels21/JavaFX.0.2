05e519fa107ef8dfd328552ccb848628d9a19cc8
BachBerg <andreas-bach-nielsen@hotmail.com>
2021-03-16 12:58:36 +0100

    16 03 2021 - Andreas Bach, ControllerPatientAkriv og tilhørende scene

---
+++ src/sample/ControllerPatientArkiv.java
@@ -0,0 +1,38 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerPatientArkiv extends ControllerArkiv implements Initializable {
+    public Label Cprlabel;
+    // interagtion med show knapper
+    //display cpr
+    // alt funktionalitet fra akrivet
+    ControllerLogin CL = new ControllerLogin();
+
+    public void PulsArkiv(ActionEvent actionEvent) throws FileNotFoundException {
+        PulsArkiv();
+    }
+
+    public void TempArkiv(ActionEvent actionEvent) throws FileNotFoundException {
+        TempArkiv();
+    }
+
+    public void SpO2Arkiv(ActionEvent actionEvent) throws FileNotFoundException {
+        SpO2Arkiv();
+    }
+
+    public void EKGArkiv(ActionEvent actionEvent) throws FileNotFoundException {
+        EKGArkiv();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Cprlabel.setText(CL.CPR);
+    }
+}
