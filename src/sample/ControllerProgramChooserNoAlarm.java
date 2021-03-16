7e2aa7cb518cd629cecf6186cc231238b22062ce
Troels21 <73639990+Troels21@users.noreply.github.com>
2021-03-16 14:17:51 +0100

    Sundhedspersonale login- Troels 16-03-2021 14:17

---
+++ src/sample/ControllerProgramChooserNoAlarm.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+
+public class ControllerProgramChooserNoAlarm {
+    Main m = new Main();
+
+    public void scene1Clicked() throws IOException {
+        Parent Puls_SpO2_Temp = FXMLLoader.load(getClass().getResource("Puls_SpO2_Temp.fxml"));
+        m.stage.setScene(new Scene(Puls_SpO2_Temp, 650, 400));
+        m.stage.show();
+    }
+
+    public void Arkiv() throws IOException {
+        Parent ControllerArkiv = FXMLLoader.load(getClass().getResource("Arkiv.fxml"));
+        m.stage.setScene(new Scene(ControllerArkiv, 650, 400));
+        m.stage.show();
+    }
+
+    public void ekgClicked() throws IOException {
+        Parent EKG = FXMLLoader.load(getClass().getResource("EKG.fxml"));
+        m.stage.setScene(new Scene(EKG, 650, 400));
+        m.stage.show();
+    }
+}
