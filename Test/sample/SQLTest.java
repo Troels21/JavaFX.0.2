8b0aa8cb3f58dd7ac3b75aab544680d80c9fb092
Schasti <imamshah19@yahoo.com>
2021-04-21 10:45:40 +0200

    10:45 sql test

--- Test/sample/SQLTest.java
+++ Test/sample/SQLTest.java
@@ -4,16 +4,19 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQLTest {
     SQL sql_ob = new SQL();
-    String CPR;
+    GenMetoder gm = new GenMetoder();
+    String  CPR;
+    double Puls, Temp, SpO2, EKG;
 
     @BeforeEach
     void setUp() {
-        ;
-        CPR="";
+        CPR="2103005445";
     }
 
     @Test
@@ -25,34 +28,62 @@ class SQLTest {
 
     @Test
     void createTableCPRPulsTest() {
+        CPR="2103005444";
+        sql_ob.createTableCPRPuls(CPR);
     }
 
     @Test
     void createTableCPREKGTest() {
+        CPR="1234321567";
+        sql_ob.createTableCPREKG(CPR);
     }
 
     @Test
     void writeToPatientMaalingPulsTest() {
+        Temp=37;
+        Puls=67;
+        SpO2=99;
+        sql_ob.writeToPatientMaalingPuls(CPR,Puls,Temp,SpO2);
+
     }
 
     @Test
     void writeToPatientMaalingEKGTest() {
+        EKG = 10;
+        sql_ob.writeToPatientMaalingEKG(CPR,EKG);
     }
 
     @Test
     void rowCounterTest() {
+        int[] row;
+        row = new int[sql_ob.rowCounter("patientMaalingEKG",CPR)];
+        for(int i =0; i<row.length; i++) {
+            System.out.println(row[i]);
+        }
     }
 
     @Test
     void readDataEKGTest() {
+        try {
+            sql_ob.readDataEKG(CPR,gm.EKGTime,gm.EKGValue);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     @Test
     void readDataPulsTest() {
+        try {
+            sql_ob.readDataPuls(CPR, gm.PulseTime, gm.PulseValue, gm.TempValue, gm.SpO2Value);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     @Test
     void readDataLogininfoTest() {
+        sql_ob.ReadDataLogininfo("DR");
+
     }
 
     @Test
